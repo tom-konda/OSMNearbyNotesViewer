55b65ac91a2cd01dfc48981828452dcbd3e11481
tom-konda <tom.konda.dev@gmail.com>
2016-10-21 14:15:56 +0900

    Remove 1 d.ts file. Update osm-auth.d.ts.

--- src/ts/browser/custom_typings/osm-auth/osm-auth.d.ts
+++ src/ts/browser/custom_typings/osm-auth/osm-auth.d.ts
@@ -1,32 +1,36 @@
 declare var osmAuth: osmAuthConstructor;
 
+declare module 'osm-auth' {
+  export = osmAuth;
+}
+
 interface osmAuthConstructor {
-  new (osmAuthConfig) : osmAuthInstance
+  new (osmAuthConfig): osmAuthInstance
 }
 
 interface osmAuthInstance {
-  logout():osmAuthInstance,
-  authenticated():boolean,
-  authenticate(callback:(err, oauth:osmAuthInstance) => any),
-  xhr(option:osmAuthXHROptions, callback:(err, xhr:any) => any)
-  options(option:osmAuthConfig),
-  bootstrapToken(oauth_token:string, callback:(err, oauth:osmAuthInstance) => any)
+  logout(): osmAuthInstance,
+  authenticated(): boolean,
+  authenticate(callback: (err: any, oauth: osmAuthInstance) => any),
+  xhr(option: osmAuthXHROptions, callback: (err, xhr: any) => any)
+  options(option: osmAuthConfig),
+  bootstrapToken(oauth_token: string, callback: (err, oauth: osmAuthInstance) => any)
 }
 
 interface osmAuthConfig {
   oauth_consumer_key: string,
   oauth_secret: string,
-  url ?: string,
+  url?: string,
   auto?: boolean,
   loading?: (any) => any,
   done?: (any) => any,
-  landing ?: string,
-  singlepage ?: boolean
+  landing?: string,
+  singlepage?: boolean
 }
 
 interface osmAuthXHROptions {
   path: string,
   method: 'POST' | 'PUT' | 'GET',
-  content ?: string,
-  options ?: {},
+  content?: string,
+  options?: {},
 }
--- src/ts/browser/custom_typings/tsd.d.ts
+++ src/ts/browser/custom_typings/tsd.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="osm-auth/osm-auth.d.ts" />
 /// <reference path="polyfill/polyfill.d.ts" />
-/// <reference path="Leaflet.Icon.Glyph/Leaflet.Icon.Glyph.d.ts" />
 /// <reference path="oauth-config/oauth-config.d.ts" />
