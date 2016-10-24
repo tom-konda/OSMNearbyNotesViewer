c51b3bebc1700412c8d4c171bedbb6a711b4393d
tom-konda <tom.konda.dev@gmail.com>
2016-10-25 00:45:11 +0900

    Add indexedDB checking function.

--- src/ts/browser/indexeddb-class.ts
+++ src/ts/browser/indexeddb-class.ts
@@ -1,27 +1,31 @@
 export namespace IDBService {
   'use strict';
-  let _db:IDBDatabase;
-  let _idbName:string;
-  let _tables:string[] = [];
+  let _db: IDBDatabase;
+  let _idbName: string;
+  let _tables: string[] = [];
 
-  export function openDB(idbName = 'example', version = 1):IDBOpenDBRequest {
+  export function enableIndexedDB() {
+    return typeof window.indexedDB !== 'undefined' ? true : false;
+  }
+
+  export function openDB(idbName = 'example', version = 1): IDBOpenDBRequest {
     _idbName = idbName;
     let idbReq = indexedDB.open(idbName, version);
     idbReq.addEventListener(
       'upgradeneeded',
-      function(event){
+      function (event) {
         _db = (<IDBRequest>event.target).result;
       }
     );
     idbReq.addEventListener(
       'success',
-      function(event){
+      function (event) {
         _db = _db || (<IDBRequest>event.target).result;
         let objStoreNameList = (<DOMStringList>(<IDBRequest>event.target).result.objectStoreNames);
         let i = 0;
         let cnt = objStoreNameList.length;
-        for( ; i < cnt; ++i){
-          if(_tables.indexOf(objStoreNameList.item(i)) === -1){
+        for (; i < cnt; ++i) {
+          if (_tables.indexOf(objStoreNameList.item(i)) === -1) {
             _tables.push(objStoreNameList.item(i));
           }
         }
@@ -29,26 +33,26 @@ export namespace IDBService {
     );
     return idbReq;
   }
-  export function dropDB(idbName = _idbName){
+  export function dropDB(idbName = _idbName) {
     indexedDB.deleteDatabase(idbName);
   }
-  export function add (dataObj) {
+  export function add(dataObj) {
     ;
   }
-  export function getTransaction(tables = _tables, mode = 'readwrite'):IDBTransaction{
+  export function getTransaction(tables = _tables, mode = 'readwrite'): IDBTransaction {
     return _db.transaction(tables, mode);
   }
-  export function createObjectStore (table, keyPath, isAutoIncrement = true):IDBObjectStore{
-      if (_tables.indexOf(table) === -1){
-        _tables.push(table);
-      }
-      return _db.createObjectStore(table, {keyPath : keyPath, autoIncrement : isAutoIncrement});
+  export function createObjectStore(table, keyPath, isAutoIncrement = true): IDBObjectStore {
+    if (_tables.indexOf(table) === -1) {
+      _tables.push(table);
+    }
+    return _db.createObjectStore(table, { keyPath: keyPath, autoIncrement: isAutoIncrement });
   }
-  export function deleteObjectStore (tables){
-      // TODO
+  export function deleteObjectStore(tables) {
+    // TODO
   }
 
-  export function getTables(){
+  export function getTables() {
     return _tables;
   }
 };
--- src/ts/browser/main.ts
+++ src/ts/browser/main.ts
@@ -4,235 +4,256 @@ import { IDBService } from './indexeddb-class';
 import { coordinateCalc } from './coordinate-calc';
 // require('leaflet.icon.glyph');
 
-(function () {
-
-  let idbService = IDBService;
-
-  let idbReq = idbService.openDB('osmNearNotes', 1);
-
-  idbReq.addEventListener(
-    'upgradeneeded',
-    function (event) {
-      let oldVersion = event.oldVersion;
-      if (oldVersion < 1) {
-        // Create new indexedDB
-        let notesStore = idbService.createObjectStore('notes', 'id', false);
-        notesStore.createIndex(
-          'createdIdx',
-          'created',
-          {
-            'unique': false,
-            'multiEntry': false,
-          }
-        );
-
-        notesStore.createIndex(
-          'modifiedIdx',
-          'modified',
-          {
-            'unique': false,
-            'multiEntry': false,
-          }
-        );
-
-        let commentsStore = idbService.createObjectStore('comments', ['noteId', 'commentNum'], false);
-        commentsStore.createIndex(
-          'noteIdIdx',
-          'noteId',
-          {
-            'unique': false,
-            'multiEntry': false,
-          }
-        );
-      }
-    },
-    false
-  );
-
-  const reactRootWrapperElement = document.querySelector('#AppWrapper');
-  const auth: osmAuthInstance = OSMOAuthConfig.OAuth;
-
-  window.addEventListener(
-    'oauthButtonClicked',
-    (event) => {
-      auth.authenticate(
-        (err, oauth) => {
-          if (err === null) {
-            const oauthReadyEvent = new CustomEvent(
-              'oauthReady',
-              {
-                detail: OSMOAuthConfig
-              }
-            )
-            reactRootWrapperElement.dispatchEvent(oauthReadyEvent)
-          }
-          else {
-            console.error(err)
-          }
-        }
-      );
+if (IDBService.enableIndexedDB() === false) {
+  document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+      const reactRootWrapperElement = document.querySelector('#AppWrapper');
+      const notQualifiedBrowserEvent = new CustomEvent('notQualifiedBrowser');
+      reactRootWrapperElement.dispatchEvent(notQualifiedBrowserEvent);
     }
   )
+}
+else {
+  (function () {
 
-  window.addEventListener(
-    'getNearbyNotesClicked',
-    (event: CustomEvent) => {
-      let receiveData = {
-        coordinate: {},
-        notes: {},
-        noteComments: {},
-      }
+    let idbService = IDBService;
 
-      const getUserHomeLocation = () => {
-        return new Promise(function (resolve, reject) {
-          auth.xhr({
-            method: 'GET',
-            path: '/api/0.6/user/details'
-          }, function (err, details) {
-            // details is an XML DOM of user details
-            if (err === null) {
-              const homeLocation = details.querySelector('osm > user > home');
-              if (homeLocation !== null) {
-                let coordinate: { lat: string, lon: string } = {
-                  lat: homeLocation.getAttribute('lat'),
-                  lon: homeLocation.getAttribute('lon'),
+    let idbReq = idbService.openDB('osmNearNotes', 1);
+
+    idbReq.addEventListener(
+      'upgradeneeded',
+      function (event) {
+        let oldVersion = event.oldVersion;
+        if (oldVersion < 1) {
+          // Create new indexedDB
+          let notesStore = idbService.createObjectStore('notes', 'id', false);
+          notesStore.createIndex(
+            'createdIdx',
+            'created',
+            {
+              'unique': false,
+              'multiEntry': false,
+            }
+          );
+
+          notesStore.createIndex(
+            'modifiedIdx',
+            'modified',
+            {
+              'unique': false,
+              'multiEntry': false,
+            }
+          );
+
+          let commentsStore = idbService.createObjectStore('comments', ['noteId', 'commentNum'], false);
+          commentsStore.createIndex(
+            'noteIdIdx',
+            'noteId',
+            {
+              'unique': false,
+              'multiEntry': false,
+            }
+          );
+        }
+      },
+      false
+    );
+
+    document.addEventListener(
+      'DOMContentLoaded',
+      () => {
+        const reactRootWrapperElement = document.querySelector('#AppWrapper');
+        const auth: osmAuthInstance = OSMOAuthConfig.OAuth;
+        const oauthReadyEvent = new CustomEvent(
+          'oauthReady',
+          {
+            detail: auth
+          }
+        )
+        reactRootWrapperElement.dispatchEvent(oauthReadyEvent)
+
+        window.addEventListener(
+          'oauthButtonClicked',
+          (event) => {
+            auth.authenticate(
+              (err, oauth) => {
+                if (err) {
+                  console.error(err)
                 }
-                resolve(coordinate);
-                receiveData.coordinate = coordinate;
-                return;
+                else {
+                  const oauthReadyEvent = new CustomEvent(
+                    'oauthReady',
+                    {
+                      detail: auth
+                    }
+                  )
+                  reactRootWrapperElement.dispatchEvent(oauthReadyEvent)
+                }
               }
-              swal('Cannot get user home location.', 'error');
-              reject('Cannot get user home location.');
-            }
-            reject(err);
-          });
-        });
-      };
-
-      const getNotes = (coordinate: { lat: string, lon: string }) => {
-        return new Promise((resolve, reject) => {
-          const edge = coordinateCalc.getCoordinateArea(coordinate, 10);
-          auth.xhr({
-            method: 'GET',
-            path: `/api/0.6/notes?limit=1000&bbox=${edge.w},${edge.s},${edge.e},${edge.n}`
-          }, function (err, details: XMLDocument) {
-            if (err === null) {
-              const notesList = details.querySelectorAll('osm > note');
-
-              if (notesList.length) {
-                resolve(notesList);
-                return;
-              }
-              swal('Cannot get comments around near your home location.', 'error');
-              reject('Cannot get comments around near your home location.');
-            }
-            reject(err);
-          });
-        });
-      };
-
-      const setNotesList = (notesXML: NodeListOf<Element>) => {
-        const trans = idbService.getTransaction();
-        const notesOS = trans.objectStore('notes');
-        const commentsOS = trans.objectStore('comments');
-
-        for (let i = 0, notesCnt = notesXML.length; i < notesCnt; ++i) {
-          let lastModified = new Date(0);
-          let noteId = notesXML[i].querySelector('id').textContent;
-
-          let commentList = notesXML[i].querySelectorAll('comments > comment');
-          for (let j = 0, commentCnt = commentList.length; j < commentCnt; ++j) {
-            let date = commentList[j].querySelector('date').textContent;
-            let text = commentList[j].querySelector('text').textContent;
-            let action = commentList[j].querySelector('action').textContent;
-
-            let user = '';
-            let userURL = '';
-            if (commentList[j].querySelector('user')) {
-              user = commentList[j].querySelector('user').textContent;
-              userURL = commentList[j].querySelector('user_url').textContent;
-            }
-
-            let commentDate = new Date(Date.parse(`${date.split(' ').slice(0, 2).join('T')}+0000`));
-            let commentData = {
-              'noteId': +noteId,
-              'commentNum': j,
-              'date': commentDate,
-              'user': user,
-              'userURL': userURL,
-              'text': text,
-              'action': action,
-            }
-            let commentResult = commentsOS.put(commentData);
-            receiveData[noteId][j] = commentData;
-
-            commentResult.addEventListener(
-              'success',
-              (event) => {
-              }
-            )
-
-            commentResult.addEventListener(
-              'error',
-              (event) => console.warn(event)
-            )
-            if (commentDate > lastModified) {
-              lastModified = commentDate;
-            }
+            );
           }
+        )
 
-          let latlng = {
-            lat: notesXML[i].getAttribute('lat'),
-            lng: notesXML[i].getAttribute('lon'),
+        window.addEventListener(
+          'getNearbyNotesClicked',
+          (event: CustomEvent) => {
+            let receiveData = {
+              coordinate: {},
+              notes: [],
+              noteComments: [],
+            }
+
+            const getUserHomeLocation = () => {
+              return new Promise(function (resolve, reject) {
+                auth.xhr({
+                  method: 'GET',
+                  path: '/api/0.6/user/details'
+                }, function (err, details) {
+                  // details is an XML DOM of user details
+                  if (err === null) {
+                    const homeLocation = details.querySelector('osm > user > home');
+                    if (homeLocation !== null) {
+                      let coordinate: { lat: string, lon: string } = {
+                        lat: homeLocation.getAttribute('lat'),
+                        lon: homeLocation.getAttribute('lon'),
+                      }
+                      resolve(coordinate);
+                      return;
+                    }
+                    swal('Cannot get user home location.', 'error');
+                    reject('Cannot get user home location.');
+                  }
+                  reject(err);
+                });
+              });
+            };
+
+            const getNotes = (coordinate: { lat: string, lon: string }) => {
+              receiveData.coordinate = coordinate;
+              return new Promise((resolve, reject) => {
+                const edge = coordinateCalc.getCoordinateArea(coordinate, 10);
+                auth.xhr({
+                  method: 'GET',
+                  path: `/api/0.6/notes?limit=1000&bbox=${edge.w},${edge.s},${edge.e},${edge.n}`
+                }, function (err, details: XMLDocument) {
+                  if (err === null) {
+                    const notesList = details.querySelectorAll('osm > note');
+
+                    if (notesList.length) {
+                      resolve(notesList);
+                      return;
+                    }
+                    swal('Cannot get comments around near your home location.', 'error');
+                    reject('Cannot get comments around near your home location.');
+                  }
+                  reject(err);
+                });
+              });
+            };
+
+            const setNotesList = (notesXML: NodeListOf<Element>) => {
+              const trans = idbService.getTransaction();
+              const notesOS = trans.objectStore('notes');
+              const commentsOS = trans.objectStore('comments');
+
+              for (let i = 0, notesCnt = notesXML.length; i < notesCnt; ++i) {
+                let noteComments = [];
+                let lastModified = new Date(0);
+                let noteId = notesXML[i].querySelector('id').textContent;
+
+                let commentList = notesXML[i].querySelectorAll('comments > comment');
+                for (let j = 0, commentCnt = commentList.length; j < commentCnt; ++j) {
+                  let date = commentList[j].querySelector('date').textContent;
+                  let text = commentList[j].querySelector('text').textContent;
+                  let action = commentList[j].querySelector('action').textContent;
+
+                  let user = '';
+                  let userURL = '';
+                  if (commentList[j].querySelector('user')) {
+                    user = commentList[j].querySelector('user').textContent;
+                    userURL = commentList[j].querySelector('user_url').textContent;
+                  }
+
+                  let commentDate = new Date(Date.parse(`${date.split(' ').slice(0, 2).join('T')}+0000`));
+                  let commentData = {
+                    'noteId': +noteId,
+                    'commentNum': j,
+                    'date': commentDate,
+                    'user': user,
+                    'userURL': userURL,
+                    'text': text,
+                    'action': action,
+                  }
+                  let commentResult = commentsOS.put(commentData);
+                  noteComments[j] = commentData;
+
+                  commentResult.addEventListener(
+                    'success',
+                    (event) => {
+                    }
+                  )
+
+                  commentResult.addEventListener(
+                    'error',
+                    (event) => console.warn(event)
+                  )
+                  if (commentDate > lastModified) {
+                    lastModified = commentDate;
+                  }
+                }
+                receiveData.noteComments[noteId] = noteComments;
+
+                let latlng = {
+                  lat: notesXML[i].getAttribute('lat'),
+                  lng: notesXML[i].getAttribute('lon'),
+                }
+
+                let created = notesXML[i].querySelector('date_created').textContent;
+                let status = notesXML[i].querySelector('status').textContent;
+                let noteData = {
+                  'latlng': latlng,
+                  'id': +noteId,
+                  'created': new Date(Date.parse(`${created.split(' ').slice(0, 2).join('T')}+0000`)),
+                  'modified': new Date(lastModified.getTime()),
+                  'status': status,
+                };
+                let notesResult = notesOS.put(noteData);
+                receiveData.notes[noteId] = noteData;
+
+                notesResult.addEventListener(
+                  'success',
+                  (event) => { }
+                )
+
+                notesResult.addEventListener(
+                  'error',
+                  (event) => console.error(event.error)
+                )
+              }
+              Promise.resolve();
+            }
+
+            getUserHomeLocation().then(
+              getNotes,
+              (error) => console.error(error)
+            ).then(
+              setNotesList,
+              (error) => console.error(error)
+              ).then(
+              () => {
+                const receiveDataEvent = new CustomEvent(
+                  'receiveNotesAndCoordinate',
+                  {
+                    detail: receiveData
+                  }
+                )
+                reactRootWrapperElement.dispatchEvent(receiveDataEvent);
+              }
+              );
           }
-
-          let created = notesXML[i].querySelector('date_created').textContent;
-          let status = notesXML[i].querySelector('status').textContent;
-          let noteData = {
-            'latlng': latlng,
-            'id': +noteId,
-            'created': new Date(Date.parse(`${created.split(' ').slice(0, 2).join('T')}+0000`)),
-            'modified': new Date(lastModified.getTime()),
-            'status': status,
-          };
-          let notesResult = notesOS.put(noteData);
-          receiveData[noteId] = noteData;
-
-          notesResult.addEventListener(
-            'success',
-            (event) => { }
-          )
-
-          notesResult.addEventListener(
-            'error',
-            (event) => console.error(event.error)
-          )
-        }
-      }
-
-      getUserHomeLocation().then(
-        getNotes,
-        (error) => console.error(error)
-      ).then(
-        setNotesList,
-        (error) => console.error(error)
         );
-
-      const receiveDataEvent = new CustomEvent(
-        'receiveNotesAndCoordinate',
-        {
-          detail: receiveData
-        }
-      )
-      reactRootWrapperElement.dispatchEvent(receiveDataEvent);
-    }
-  );
-
-  const oauthReadyEvent = new CustomEvent(
-    'oauthReady',
-    {
-      detail: OSMOAuthConfig
-    }
-  )
-  reactRootWrapperElement.dispatchEvent(oauthReadyEvent)
-})();
+      }
+    )
+  })();
+}
