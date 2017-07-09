c80cc1105254ffa196c5eb1708e5a34faa9d0b68
tom-konda <tom.konda.dev@gmail.com>
2017-07-09 18:58:10 +0900

    Add js config file.

---
+++ config/default.config.js
@@ -0,0 +1,8 @@
+export default {
+  "oauth_consumer_key": "",
+  "oauth_secret": "",
+  "url" : "http://www.openstreetmap.org",
+  "auto" : true,
+  "landing" : "land.html",
+  "singlepage" : false,
+}
